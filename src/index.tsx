df2928af57b20307090d815ddfab5f2885d305ef
Fastovec <sergejfastovec284@gmail.com>
2022-08-12 17:00:36 +0300

    added absolute imports and dummy App

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './container/App/App'
 
-const h1 = <h1>hello word</h1>;
 
-function App() {
-    return (
-        <div>
-            <div>
-            {h1}
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quibusdam. Atque corporis ullam maiores iusto velit adipisci, labore aliquid nam.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ullam porro, assumenda ducimus quas expedita!</p>
-            </div>
-        </div>
-    )
-}
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(<App />);
-
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<App />)
