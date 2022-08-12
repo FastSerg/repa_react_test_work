d3b3415a42cf15de0d507d1954ea85d27eae72d0
Fastovec <sergejfastovec284@gmail.com>
2022-08-12 17:25:38 +0300

    install mui/material and svg icon

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,30 +1,9 @@
 import React from 'react'
 
-type Props = {
-    title: string
-}
-
-const AppList = () => {
-    return (
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
-    )
-}
-
-const AppHeader = (props: Props) => {
-    return <h1>Hello {props.title}</h1>
-}
-
 const App = () => {
     return (
         <>
-            <AppHeader title="App.js" />
-            <AppHeader title="React js" />
-            <AppHeader />
-            <AppList />
+            <h1>Hello App</h1>
         </>
     )
 }
