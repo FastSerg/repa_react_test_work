bb308a8d7c5bced4802d7b7127d286f366fdffcb
Fastovec <sergejfastovec284@gmail.com>
2022-08-12 13:23:58 +0300

    added App

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const h1 = <h1>hello word</h1>;
+
+function App() {
+    return (
+        <div>
+            {h1}
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quibusdam. Atque corporis ullam maiores iusto velit adipisci, labore aliquid nam.</p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render('hello wprd');
+root.render(<App />);
 
