379b62cf8d8c5a80497d8bf98ee5da95c57760c8
Fastovec <sergejfastovec284@gmail.com>
2022-08-12 13:43:08 +0300

    added react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,13 @@ const h1 = <h1>hello word</h1>;
 function App() {
     return (
         <div>
+            <div>
             {h1}
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, quibusdam. Atque corporis ullam maiores iusto velit adipisci, labore aliquid nam.</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ullam porro, assumenda ducimus quas expedita!</p>
+            </div>
         </div>
     )
 }
